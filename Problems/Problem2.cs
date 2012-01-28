715e68e88abc919b6d3d81ed424f4514fec4ec96
unknown <bwillard@bwillard-laptop.(none)>
2012-01-27 21:45:08 -0600

    solve problem #2

---
+++ Problems/Problem2.cs
@@ -0,0 +1,14 @@
+﻿using ProjectEuler.Utilities;
+using System.Linq;
+
+namespace ProjectEuler.Problems
+{
+    class Problem2 : IProblem
+    {
+        public int Run()
+        {
+            return new Fibonacci(4000000).Where(n => n.IsDivisiableBy(2)).Sum();
+        }
+
+    }
+}
---
+++ Utilities/Fibonacci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ProjectEuler.Utilities
+{
+    class Fibonacci : IEnumerable<int>
+    {
+        private int m_max;
+        private int m_last1=1;
+        private int m_last2=1;
+
+        public Fibonacci(int max)
+        {
+            m_max = max;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return 0;
+            yield return 1;
+
+            int next = 1;
+            while (next<m_max)
+            {
+                yield return next;
+                m_last2 = m_last1;
+                m_last1 = next;
+                next = m_last1 + m_last2;
+            } 
+        }
+    }
+}
