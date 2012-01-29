b10166a080fffe9c092b16297ed3b6e37f5bbba5
unknown <bwillard@bwillard-laptop.(none)>
2012-01-29 16:25:08 -0600

    solve problem 7

---
+++ Problems/Problem7.cs
@@ -0,0 +1,25 @@
+﻿using ProjectEuler.Utilities;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler.Problems
+{
+    class Problem7 : IProblem
+    {
+        private long m_primeNumber;
+
+        public Problem7(long primeNumber)
+        {
+            m_primeNumber = primeNumber;
+        }
+
+        public long Run()
+        {
+            IEnumerable<long> primes = new Primes(long.MaxValue);
+
+            return primes.ElementAt((int)m_primeNumber-1);
+        }
+
+    }
+}
---
+++ Tests/Problem7Test.cs
@@ -0,0 +1,23 @@
+﻿
+using NUnit.Framework;
+using ProjectEuler.Problems;
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    public class Problem7Test
+    {
+        [Test]
+        public void Sample()
+        {
+            IProblem p = new Problem7(6);
+            Assert.AreEqual(13, p.Run());
+        }
+
+        [Test]
+        public void Problem()
+        {
+            IProblem p = new Problem7(10001);
+            Assert.AreEqual(104743, p.Run());
+        }
+    }
+}
