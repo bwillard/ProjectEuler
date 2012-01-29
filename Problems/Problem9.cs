19db1cc9c2ed18594caee5d88f6798dfb3219455
unknown <bwillard@bwillard-laptop.(none)>
2012-01-29 16:52:16 -0600

    solve problem 10

---
+++ Problems/Problem9.cs
@@ -0,0 +1,37 @@
+﻿using ProjectEuler.Utilities;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler.Problems
+{
+    class Problem9 : IProblem
+    {
+        private long m_sum;
+
+        public Problem9(long sum)
+        {
+            m_sum = sum;
+        }
+
+        public long Run()
+        {
+            for (int i = 1; i < m_sum / 2; i++)
+            {
+                for (int j = i; j < m_sum; j++)
+                {
+                    long product = i * i + j * j;
+                    long sqrt = (long)Math.Sqrt(product);
+
+                    if (sqrt * sqrt == product && i + j + sqrt == m_sum)
+                    {
+                        return i * j * sqrt;
+                    }
+                }
+            }
+
+            throw new Exception("Couldn't find a matching set of numbers");
+        }
+
+    }
+}
---
+++ Tests/Problem9Test.cs
@@ -0,0 +1,24 @@
+﻿
+using NUnit.Framework;
+using ProjectEuler.Problems;
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    public class Problem9Test
+    {
+        [Test]
+        public void Sample()
+        {
+            //3^2 + 4^2 = 5^2
+            IProblem p = new Problem9(12);
+            Assert.AreEqual(60, p.Run());
+        }
+
+        [Test]
+        public void Problem()
+        {
+            IProblem p = new Problem9(1000);
+            Assert.AreEqual(31875000, p.Run());
+        }
+    }
+}
